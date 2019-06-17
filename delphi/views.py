fe0eeceea0312ad5b27160d5f327d0c6ede60d87
kevb10 <gasydev@gmail.com>
2019-06-17 09:02:34 -0500

    little cleanup + gitignore update

--- delphi/views.py
+++ delphi/views.py
@@ -9,11 +9,14 @@ import matplotlib.pyplot as plt
 import matplotlib
 
 def index(request):
+  # TODO: Create a simple landing page
   return HttpResponse("WELCOME", content_type='application/json')
 
 def predict(request, ticker_name):
   company = Stocker(ticker=ticker_name)
   data = company.retrieve_stock_data(start_date=None, end_date=None, stats=['Adj. Close'], plot_type='basic')
-  json = data.to_json(orient='split') 
+  json = data.to_json(orient='split')
+
+  print(company.create_prophet_model())
 
   return HttpResponse(json, content_type='application/json')
