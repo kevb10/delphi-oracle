8d3e0741d47f9d3b3bae7bd72077eb92a4b41778
kevb10 <gasydev@gmail.com>
2019-09-19 07:45:31 -0500

    color code direction

--- main.py
+++ main.py
@@ -8,17 +8,20 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 def main():
-    prediction_result = [] * 5
+    prediction_estimate = [] * 5
+    prediction_direction = [] * 5
     result = {}
 
     if request.method == 'POST':
         symbol = request.form.get('symbol')
         stock = Oracle(symbol,  requests.Session())
         prediction = stock.predict_future(days=5)
-        prediction_result = prediction['estimate'].values
+        prediction_estimate = prediction['estimate'].values
+        prediction_direction = prediction['direction'].values
         result["ticker"] = symbol.upper()
 
-    result["prediction"] = prediction_result
+    result["prediction"] = prediction_estimate
+    result["direction"] = prediction_direction
     
     return render_template('main.html', future=result)
 
